72e5c592a8e3ef97667ab1532f749b2ae47461e6
Talha Ansari <talhaansari@Talhas-MacBook-Pro.local>
2021-11-19 19:59:42 -0500

    test runner

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -1,2 +1,14 @@
-package runner;public class TestRunner {
+package runner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {"pretty", "html:target/site/cucumber-pretty.html"},
+        features ={"src/test/resources/features"},
+        glue = {"stepdefs"},
+        //  tags = "@addtocart",
+        dryRun = false
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
